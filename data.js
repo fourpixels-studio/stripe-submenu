aa0c708cd222014df1f8d288cf0fb29d7305d742
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2024-10-25 21:14:37 +0300

    First commit 🚀

---
+++ data.js
@@ -0,0 +1,28 @@
+const sublinks = [
+    {
+        page: 'products',
+        links: [
+        { label: 'payment', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'terminal', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'connect', icon: 'fas fa-credit-card', url: '/products' },
+        ],
+    },
+    {
+        page: 'developers',
+        links: [
+        { label: 'plugins', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'libraries', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'help', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'billing', icon: 'fas fa-credit-card', url: '/products' },
+        ],
+    },
+    {
+        page: 'company',
+        links: [
+        { label: 'about', icon: 'fas fa-credit-card', url: '/products' },
+        { label: 'customers', icon: 'fas fa-credit-card', url: '/products' },
+        ],
+    },
+];
+
+export default sublinks;
